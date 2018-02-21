2c3dae756f0ee78dabddc8aa69eab102a9f77b2a
Michael <michael.brian.wong@gmail.com>
2018-02-20 19:34:20 -0700

    Add Database Cleaner

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Welcome Controller", type: :request do
   describe "Welcome Controller" do
-    it "returns a correct http response" do
+    it "returns a valid http response" do
       get '/'
 
       expect(response).to have_http_status(200)
